f42282855c4853eab6db404836ad82d912df7fd2
Jacksoften <liyunzhejack@gmail.com>
2018-06-29 09:44:28 -0700

    linear kernel works

--- SVM/run_SVM.py
+++ SVM/run_SVM.py
@@ -3,7 +3,7 @@ from mnist import MNIST
 import numpy as np
 from sklearn import svm
 
-mndata = MNIST('samples')
+mndata = MNIST('../samples')
 Xtrain, ytrain = mndata.load_training()
 Xtest, ytest = mndata.load_testing()
 
@@ -13,21 +13,23 @@ XtestArr = np.array(Xtest)
 ytestArr = np.array(ytest)
 
 # take subsets to run svm
-indexesList = [np.random.choice(range(60000), size=s, replace=False) for s in [100, 1000, 10000, 20000, 60000]]
+indexesList = [np.random.choice(range(60000), size=s, replace=False) \
+               # for s in [100, 1000, 10000, 20000, 30000, 40000, 50000, 60000]]
+	       for s in [10000, 20000, 30000, 40000, 50000, 60000]]
 
 from time import time
 
 for indexes in indexesList:
+    print('\n', "-------------------------", '\n')
     starttime = time()
     XtrainTemp = XtrainArr[indexes]
     ytrainTemp = ytrainArr[indexes]
 
     print(("training size: {}").format(len(indexes)))
     # print("start training")
-    clf = svm.SVC(kernel='linear')
+    # clf = svm.SVC(kernel='linear')
+    clf = svm.LinearSVC(C=1)
     clf.fit(XtrainTemp, ytrainTemp)
-    
-    # print("start testing")
     predictions = clf.predict(XtestArr)
     correct = sum(predictions == ytestArr)
     
@@ -35,4 +37,3 @@ for indexes in indexesList:
     
     print(("Accuracy: {}").format(acc))
     print(("Time spent: {}").format(time() - starttime))
-    print('\n', "-------------------------", '\n')
